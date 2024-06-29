6b8d10c2a9756270c0b908a42528494a17396fdc
DaiGuard <zenith.or.w@gmail.com>
2024-06-29 14:02:43 +0900

    コントローラの追加

--- Assets/Scripts/Controllers/RobotController.cs
+++ Assets/Scripts/Controllers/RobotController.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using Unity.Robotics.ROSTCPConnector;
+using RosMessageTypes.Geometry;
 
 public class RobotController : MonoBehaviour
 {
@@ -24,10 +26,19 @@ public class RobotController : MonoBehaviour
     [SerializeField]
     private ArticulationBody leftDriveWheel;
 
+    [SerializeField]
+    private string cmd_vel_name = "/cmd_vel";
+    private string current_vel_name = "/current_vel";
+
+    private ROSConnection ros;
+    private TwistMsg cmd_vel;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        ros = ROSConnection.GetOrCreateInstance();
+        ros.RegisterPublisher<TwistMsg>(current_vel_name);
+        ros.Subscribe<TwistMsg>(cmd_vel_name, CallbackCmdVel);
     }
 
     // Update is called once per frame
@@ -35,8 +46,16 @@ public class RobotController : MonoBehaviour
     {
         var gamepad = Gamepad.current;
         if (gamepad != null) {
-            targetSteer = gamepad.rightStick.x.ReadValue() * steerRatio;
-            targetVelocity = gamepad.leftStick.y.ReadValue() * velocityRatio;
+            if(gamepad.triangleButton.ReadValue() < 1.0)
+            {
+                targetSteer = gamepad.rightStick.x.ReadValue() * steerRatio;
+                targetVelocity = gamepad.leftStick.y.ReadValue() * velocityRatio;
+            }
+            else 
+            {
+                targetSteer = (float)cmd_vel.angular.z * steerRatio;
+                targetVelocity = (float)cmd_vel.linear.x * velocityRatio;
+            }
         }
         else {
             targetSteer = 0.0f;
@@ -48,5 +67,16 @@ public class RobotController : MonoBehaviour
 
         rightDriveWheel.SetDriveTargetVelocity(ArticulationDriveAxis.X, targetVelocity);
         leftDriveWheel.SetDriveTargetVelocity(ArticulationDriveAxis.X, targetVelocity);
+
+        var msg = new TwistMsg();
+        msg.linear.x = targetVelocity / velocityRatio;
+        msg.angular.z = targetSteer / steerRatio;
+
+        ros.Publish(current_vel_name, msg);
+    }
+
+    void CallbackCmdVel(TwistMsg msg) 
+    {
+        cmd_vel = msg;
     }
 }
